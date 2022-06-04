6304b75a2866015a35ef7795affa8b8fec7b4694
itschip <onlygamernerd@gmail.com>
2022-06-04 13:13:41 +0200

    feat(exlorer): map out files and folders

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,20 @@ windows_subsystem = "windows"
 )]
 
 use std::fs;
+use std::path::Path;
 use tauri::api::dialog::FileDialogBuilder;
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::api::Error::Path as OtherPath;
 
 #[derive(Clone, serde::Serialize)]
-struct Payload {
+struct FilePayload {
     file_name: String,
     value: String,
+}
+
+#[derive(Clone, serde::Serialize)]
+struct FolderPayload {
+    files: Vec<String>,
 }
 
 fn main() {
@@ -22,7 +29,7 @@ fn main() {
     );
 
     let new_file = CustomMenuItem::new("new_file".to_string(), "New File");
-    let new_folder = CustomMenuItem::new("new_folder".to_string(), "New Folder");   
+    let new_folder = CustomMenuItem::new("new_folder".to_string(), "New Folder");
     let file_menu = Submenu::new("File", Menu::new().add_item(new_file).add_item(new_folder));
 
     let menu = Menu::new().add_submenu(app_menu).add_submenu(file_menu);
@@ -38,7 +45,7 @@ fn main() {
                     .window()
                     .emit(
                         "file_open",
-                        Payload {
+                        FilePayload {
                             file_name: file_name.into(),
                             value: file_value.to_string(),
                         },
@@ -48,10 +55,24 @@ fn main() {
             "new_folder" => FileDialogBuilder::new().pick_folder(move |folder_path| {
                 let folder_name = folder_path.unwrap().as_path().display().to_string();
 
+                let mut found_files: Vec<String> = Vec::new();
+
                 let files = fs::read_dir(folder_name).unwrap();
                 for file in files {
-                    println!("Name: {}", file.unwrap().path().display())
+                    let f = file.unwrap().path();
+                    let file_name = f.file_name().unwrap();
+
+                    println!("{}", file_name.to_string_lossy());
+
+                    found_files.push(file_name.to_string_lossy().parse().unwrap());
                 }
+
+                event.window().emit(
+                    "folder_open",
+                    FolderPayload {
+                        files: found_files.into()
+                    },
+                ).unwrap();
             }),
             _ => {}
         })
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
-import Editor from "@monaco-editor/react";
-import { listen } from '@tauri-apps/api/event';
+import Editor, { Monaco } from "@monaco-editor/react";
+import { emit, listen } from '@tauri-apps/api/event';
+import { VSCodeButton, VSCodePanels, VSCodePanelView } from '@vscode/webview-ui-toolkit/react'
 
 function App() {
-  const [code, setCode] = useState(``);
-
-  listen('file_open', (msg: any) => {
-      setCode(msg.payload.value);
-    }) 
-
-  return (
-    <div className="App">
-      <Editor
-        height="90vh"
-        defaultLanguage="typescript"
-        value={code}
-        theme='vs-dark'
-      />
-    </div>
-  );
+	const [code, setCode] = useState(``);
+	const [files, setFiles] = useState<string[]>([]);
+	const [selectedFile, setSelectedFile] = useState<string | null>(null);
+	const monacoRef = useRef(null);
+	
+	
+	const handleEditorWillMount = (monaco: Monaco) => {
+		monaco.languages.typescript.javascriptDefaults.setEagerModelSync(true);
+	}
+	
+	/*loader.config({ monaco })*/
+	
+	listen('file_open', (msg: any) => {
+		setCode(msg.payload.value);
+	}).then(() => {
+		console.log("Opened file")
+	});
+	
+	listen<{ files: string[] }>('folder_open', (data) => {
+		console.log(data.payload)
+		setFiles(data.payload.files);
+	}).then(() => {
+		console.log("Opened folder")
+	})
+	
+	const handleOpenFolder = async () => {
+		await emit('new_folder');
+	}
+	
+	const regEx = new RegExp(/^[^.]*$/)
+	
+	return (
+		<div className="App">
+			<div className="explorer">
+				{!files.length && <div className="openfolder">
+					<VSCodeButton onClick={handleOpenFolder}>
+						Open folder
+						<span slot="start" className="codicon codicon-folder"/>
+					</VSCodeButton>
+				</div>}
+				<div className="file-list">
+					<h3>Files</h3>
+					{files.map((file) => {
+						return (
+							<>
+								<p className={selectedFile === file ? "file-item-selected" : "file-item"}>
+									{!file.match(regEx) && <span slot="start" className="codicon codicon-file-code"/>}
+									{file}
+									{file.match(regEx) && <span slot="start" className="codicon codicon-chevron-down"/>}
+								</p>
+							</>
+						)
+					})}
+				</div>
+			</div>
+			<div style={{ width: '100%' }}>
+				<VSCodePanels style={{ backgroundColor: '#232323' }}>
+					<VSCodePanelView id="view-1">
+						<Editor
+							width="100%"
+							height="100vh"
+							defaultLanguage="typescript"
+							beforeMount={handleEditorWillMount}
+							value={code}
+							theme='vs-dark'
+						/>
+					</VSCodePanelView>
+				</VSCodePanels>
+			</div>
+		</div>
+	);
 }
 
 export default App;
